67ab862cce89c158b20629ddcbe8de2e245ad256
Giannis Nikolopoulos <j.nikolo@gmail.com>
2022-12-09 14:52:36 +0200

    Added data generation scripts.

---
+++ mosquitto/generate_data.py
@@ -0,0 +1,36 @@
+import datetime,random
+
+# returns the new temperature value and the string to be sent via MQTT
+def generate_TH(previous=20,time=datetime.datetime.now()):
+    new = previous+random.uniform(-2.1,2.1)
+    if new>35.0:
+        new=35.0
+    if new<12.0:
+        new=12.0
+    return new,time.strftime("%Y-%m-%d %H:%M:%S")+"|"+str(new)[:5]
+
+def generateHVAC(time=datetime.datetime.now()):
+    new = random.randint(0,100)
+    return time.strftime("%Y-%m-%d %H:%M:%S")+"|"+str(new)[:5]
+
+def generateMiAC(device = 1,time = datetime.datetime.now()):
+    if device == 1:
+        new = random.randint(0,150)
+    else:
+        new = random.randint(0,200)
+    return time.strftime("%Y-%m-%d %H:%M:%S")+"|"+str(new)[:5]
+
+def generateEtot(previous, time = datetime.datetime.now()):
+    new = previous + 2600*24 +random.randint(-1000,1000)
+    return new,time.strftime("%Y-%m-%d %H:%M:%S")+"|"+str(new)
+
+def generateW1(time=datetime.datetime.now()):
+    new = random.random()
+    return time.strftime("%Y-%m-%d %H:%M:%S")+"|"+str(new)[:5]
+
+def generateWtot(previous,time=datetime.datetime.now()):
+    new = previous + 110 +random.randint(-10,10)
+    return new,time.strftime("%Y-%m-%d %H:%M:%S")+"|"+str(new)
+
+def generateMov1(time=datetime.datetime.now()):
+    return time.strftime("%Y-%m-%d %H:%M:%S")+"|"+str(1)

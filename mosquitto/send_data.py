268c53943763cc3f4de44619f10a3e56820dc177
Giannis Nikolopoulos <j.nikolo@gmail.com>
2023-02-27 21:54:24 +0200

    Made retain policy of messages customisable.

--- mosquitto/send_data.py
+++ mosquitto/send_data.py
@@ -18,7 +18,7 @@ def send_data(data,topic="test/",address="localhost"):
 def send_readings(stop:int = math.inf, step:int=1, start:datetime = datetime.datetime.now(),\
     address="localhost",homeTopic="/home",
     quarterTopic="/15min",dayTopic="/day",moveSensorTopic = "/movement",
-    withSleep=True, delay:float=1.0,qos:int=1):
+    withSleep=True, delay:float=1.0,qos:int=1,retain=False):
     th1 = 20
     th2 = 20
     Etot = 0
@@ -34,43 +34,43 @@ def send_readings(stop:int = math.inf, step:int=1, start:datetime = datetime.dat
         ## Generate TH[1,2] data using previous readings and append
         th1,th1data = generate_TH(th1,current_time)
         messages.append({"topic":homeTopic+quarterTopic+"/TH/TH1",\
-            "payload":th1data,"qos":qos,"retain":True})
+            "payload":th1data,"qos":qos,"retain":retain})
         th2,th2data = generate_TH(th2,current_time)
         messages.append({"topic":homeTopic+quarterTopic+"/TH/TH2",\
-            "payload":th2data,"qos":qos,"retain":True})
+            "payload":th2data,"qos":qos,"retain":retain})
         ## Generate HVAC[1,2] data and append
         messages.append({"topic":homeTopic+quarterTopic+"/HVAC/HVAC1",\
-            "payload":generateHVAC(current_time),"qos":qos,"retain":True})
+            "payload":generateHVAC(current_time),"qos":qos,"retain":retain})
         messages.append({"topic":homeTopic+quarterTopic+"/HVAC/HVAC2",\
-            "payload":generateHVAC(current_time),"qos":qos,"retain":True})
+            "payload":generateHVAC(current_time),"qos":qos,"retain":retain})
         ## Generate MiAC[1,2] readings and append
         messages.append({"topic":homeTopic+quarterTopic+"/MiAC/MiAC1",\
-            "payload":generateMiAC(device=1,time=current_time),"qos":qos,"retain":True})
+            "payload":generateMiAC(device=1,time=current_time),"qos":qos,"retain":retain})
         messages.append({"topic":homeTopic+quarterTopic+"/MiAC/MiAC2",\
-            "payload":generateMiAC(device=2,time=current_time),"qos":qos,"retain":True})
+            "payload":generateMiAC(device=2,time=current_time),"qos":qos,"retain":retain})
         ## Generate W1 reading and append.
         W1data = generateW1(time=current_time)
         messages.append({"topic":homeTopic+quarterTopic+"/W/W1",\
-            "payload":W1data,"qos":qos,"retain":True})
+            "payload":W1data,"qos":qos,"retain":retain})
         ## Generate W1 late data every 20 seconds and append
         if i%20==0:
             print("Generating late W1 data.")
             W1data = generateW1(time=current_time-datetime.timedelta(days=1)\
                 +datetime.timedelta(seconds=random.randint(1,59),minutes=random.randint(0,10)))
             messages.append({"topic":homeTopic+quarterTopic+"/W/W1",\
-            "payload":W1data,"qos":qos,"retain":True})
+            "payload":W1data,"qos":qos,"retain":retain})
         ## Generate VERY late W1 data every 120 seconds and append
         if i%120==0:
             print("Generating VERY late W1 data.")
             W1data = generateW1(time=current_time-datetime.timedelta(days=10)\
                 +datetime.timedelta(seconds=random.randint(1,59),minutes=random.randint(0,10)))
             messages.append({"topic":homeTopic+quarterTopic+"/W/W1",\
-            "payload":W1data,"qos":qos,"retain":True})
+            "payload":W1data,"qos":qos,"retain":retain})
         ## Randomly generate Mov1 readings and append
         if random.random()>0.5+0.1*movTriggered:
             #print(0.1*movTriggered,movTriggered)
             messages.append({"topic":homeTopic+moveSensorTopic+"/Mov1",\
-            "payload":generateMov1(time=current_time),"qos":qos,"retain":True})
+            "payload":generateMov1(time=current_time),"qos":qos,"retain":retain})
             movTriggered+=1
             print(f"Triggered Mov1. Times today: {movTriggered}")
         ## on the final reading for each day, generate daily
@@ -79,10 +79,10 @@ def send_readings(stop:int = math.inf, step:int=1, start:datetime = datetime.dat
             print("Generating daily meter data.")
             Etot,Etotdata = generateEtot(Etot,time=current_time-datetime.timedelta(days=1))
             messages.append({"topic":homeTopic+dayTopic+"/Etot",\
-                "payload":Etotdata,"qos":qos,"retain":True})
+                "payload":Etotdata,"qos":qos,"retain":retain})
             Wtot,Wtotdata = generateWtot(Wtot,time=current_time-datetime.timedelta(days=1))
             messages.append({"topic":homeTopic+dayTopic+"/Wtot",\
-                "payload":Wtotdata,"qos":qos,"retain":True})  
+                "payload":Wtotdata,"qos":qos,"retain":retain})  
             ## Reset times Mov1 was triggered
             movTriggered = 0
             current_day=current_time.day      
